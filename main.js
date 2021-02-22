af8c26e734a84381012834aa4115a8c75c525ce1
Nischitha07 <71720078+Nischitha07@users.noreply.github.com>
2021-02-22 19:51:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,56 @@
+status = " ";
+objects = [ ];
+function preload()
+{
+//initial_img = loadImage('dog_cat.jpg');//
+}
+function setup()
+{
+canvas = createCanvas(400,300);
+canvas.center();
+
+video = createCapture(VIDEO);
+video.hide();
+
+object_detector = ml5.objectDetector('cocossd' , modelLoaded);
+document.getElementById("status_btn").innerHTML = "Status : Detecting Objects";
+}
+function draw()
+{
+image(video ,0,0,400,300);
+
+if (status != " ")
+{ 
+r = random(255);
+g = random(255);
+b = random(255);
+object_detector.detect(video , gotResults);
+    for(var i = 0;i<objects.length;i++)
+    {
+    document.getElementById("status_btn").innerHTML = "Status : Objects Detected";
+    document.getElementById("obj_btn").innerHTML = "Number of objects : " + objects.length;
+    fill(r,g,b);
+    noFill();
+    stroke(r,g,b);
+    accuracy = floor(objects[i].confidence * 100);
+    text(objects[i].label + " " + accuracy + " % " , objects[i].x,objects[i].y);
+    rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+    }
+}
+}
+function modelLoaded()
+{
+    console.log("coco intialized");
+    status = true;
+    object_detector.detect(video , gotResults);
+}
+function gotResults(error , results)
+{ if(error)
+    {
+    console.error(error);
+    }
+    else{
+        console.log(results);
+        objects = results;
+    }
+}
